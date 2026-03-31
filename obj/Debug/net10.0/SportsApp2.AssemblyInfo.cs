44ca1af7e126d121780020fdd92006f6bb0ab1af
Nicholas Chapman-Miller <nicholaschapmanm@gmail.com>
2026-03-31 17:41:13 -0400

    comments on team and player pages

--- obj/Debug/net10.0/SportsApp2.AssemblyInfo.cs
+++ obj/Debug/net10.0/SportsApp2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsApp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6905d955ea6fd13b7d8178f4d072329962366c04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5fd1c72d05c374a48c5b2e8344adc6f752ce52d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsApp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsApp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
